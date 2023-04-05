4a9b42e78b3d53104d71ad7aa7eb1416b2fcad5b
Brando <fong.m.brandon97@gmail.com>
2023-04-05 11:40:41 -0700

    Adding description

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Chat Server
+
+## Description
+I always wanted to make a chat server. I want to make the experience easy such that people that want to chat can just use nc or telnet
+
--- src/main.cpp
+++ src/main.cpp
@@ -3,73 +3,41 @@
  * date: 6/28/22
  */
 
-// Server side C/C++ program to demonstrate Socket
-// programming
-#include <netinet/in.h>
+#include <netinet/in.h> //structure for storing address information
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <unistd.h>
-
-#define PORT 8080
-
-int main(int argc, char * argv[]) {
-	printf("Hello world!\n");
-	return 0;
-}
-
-int soc() {
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = { 0 };
-    const char* hello = "Hello from server";
+#include <sys/socket.h> //for socket APIs
+#include <sys/types.h>
   
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
+int main(int argc, char const * argv[]) {
   
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET,
-                   SO_REUSEADDR | SO_REUSEPORT, &opt,
-                   sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    // create server socket similar to what was done in
+    // client program
+    int servSockD = socket(AF_INET, SOCK_STREAM, 0);
   
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr*)&address,
-             sizeof(address))
-        < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    if ((new_socket
-         = accept(server_fd, (struct sockaddr*)&address,
-                  (socklen_t*)&addrlen))
-        < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-    valread = read(new_socket, buffer, 1024);
-    printf("%s\n", buffer);
-    send(new_socket, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
+    // string store data to send to client
+    char serMsg[255] = "Message from the server to the "
+                       "client \'Hello Client\' ";
   
-    // closing the connected socket
-    close(new_socket);
-    // closing the listening socket
-    shutdown(server_fd, SHUT_RDWR);
+    // define server address
+    struct sockaddr_in servAddr;
+  
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htons(9001);
+    servAddr.sin_addr.s_addr = INADDR_ANY;
+  
+    // bind socket to the specified IP and port
+    bind(servSockD, (struct sockaddr*)&servAddr,
+         sizeof(servAddr));
+  
+    // listen for connections
+    listen(servSockD, 1);
+  
+    // integer to hold client socket.
+    int clientSocket = accept(servSockD, NULL, NULL);
+  
+    // send's messages to client socket
+    send(clientSocket, serMsg, sizeof(serMsg), 0);
+  
     return 0;
 }
