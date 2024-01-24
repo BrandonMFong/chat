7a27c8d274b641e6da99b4226c624a094039135b
Brando <fong.m.brandon97@gmail.com>
2024-01-24 12:27:19 -0800

    can send/recv

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <netinet/in.h> //structure for storing address information 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <sys/socket.h> //for socket APIs 
+#include <sys/types.h> 
 
 #define ARGUMENT_SERVER "server"
 #define ARGUMENT_CLIENT "client"
@@ -41,6 +46,9 @@ int ArgumentsRead(int argc, char * argv[], char * mode) {
 	return 0;
 }
 
+int ServerRun();
+int ClientRun();
+
 int main(int argc, char * argv[]) {
 	int result = 0;
 	char mode = 0;
@@ -50,12 +58,75 @@ int main(int argc, char * argv[]) {
 		Help(argv[0]);
 	} else {
 		if (mode == CHAT_MODE_SERVER) {
-			printf("server\n");
+			result = ServerRun();
 		} else if (mode == CHAT_MODE_CLIENT) {
-			printf("client\n");
+			result = ClientRun();
 		}
 	}
 
 	return result;
+}
+
+int ClientRun() {
+	printf("client\n");
+	int sockD = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in servAddr;
+
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port
+        = htons(9001); // use some unused port number
+    servAddr.sin_addr.s_addr = INADDR_ANY;
+
+    int connectStatus
+        = connect(sockD, (struct sockaddr*)&servAddr,
+                  sizeof(servAddr));
+
+    if (connectStatus == -1) {
+        printf("Error...\n");
+    }
+
+    else {
+        char strData[255];
+
+        recv(sockD, strData, sizeof(strData), 0);
+
+        printf("Message: %s\n", strData);
+    }
+
+	return 0;
+}
+
+int ServerRun() {
+	printf("server\n");
+	// create server socket similar to what was done in
+    // client program
+    int servSockD = socket(AF_INET, SOCK_STREAM, 0);
+
+    // string store data to send to client
+    char serMsg[255] = "Message from the server to the "
+                       "client \'Hello Client\' ";
+
+    // define server address
+    struct sockaddr_in servAddr;
+
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htons(9001);
+    servAddr.sin_addr.s_addr = INADDR_ANY;
+
+    // bind socket to the specified IP and port
+    bind(servSockD, (struct sockaddr*)&servAddr,
+         sizeof(servAddr));
+
+    // listen for connections
+    listen(servSockD, 1);
+
+    // integer to hold client socket.
+    int clientSocket = accept(servSockD, NULL, NULL);
+
+    // send's messages to client socket
+    send(clientSocket, serMsg, sizeof(serMsg), 0);
+
+	return 0;
 }
 
