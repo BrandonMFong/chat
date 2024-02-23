46d0acefc670b5f3b3d5a76229198bbf890b57d1
Brando <fong.m.brandon97@gmail.com>
2024-02-22 20:01:14 -0800

    Adding note

--- src/protocol.hpp
+++ src/protocol.hpp
@@ -6,6 +6,10 @@
 #ifndef PROTOCOL_HPP
 #define PROTOCOL_HPP
 
+/**
+ * protocol used with be SIMPLE
+ * ref: https://en.wikipedia.org/wiki/Session_Initiation_Protocol
+ */
 namespace Protocol {
 
 int Handshake(int d);
