ef68231b00e42e2ab49068888bf91f38d42a98d6
Brando <fong.m.brandon97@gmail.com>
2024-03-07 09:00:29 -0800

    Testing op

--- testbench/command_tests.hpp
+++ testbench/command_tests.hpp
@@ -29,12 +29,28 @@ int test_commandinit() {
 	return result;
 }
 
+int test_commandop() {
+	UNIT_TEST_START;
+	int result = 0;
+
+	InputBuffer buf("command subcommand arg0 arg1");
+	Command c(buf);
+
+	if (strcmp(c.op(), "command")) {
+		result = 1;
+	}
+
+	UNIT_TEST_END(!result, result);
+	return result;
+}
+
 void command_tests(int * pass, int * fail) {
 	int p = 0, f = 0;
 	
 	INTRO_TEST_FUNCTION;
 
 	LAUNCH_TEST(test_commandinit, p, f);
+	LAUNCH_TEST(test_commandop, p, f);
 
 	if (pass) *pass += p;
 	if (fail) *fail += f;
