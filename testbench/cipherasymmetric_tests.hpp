84c8e6924724188a2fc1252595b109df9db937b6
Brando <fong.m.brandon97@gmail.com>
2024-07-10 13:25:23 -0700

    good encryption/decryption

--- testbench/cipherasymmetric_tests.hpp
+++ testbench/cipherasymmetric_tests.hpp
@@ -20,7 +20,7 @@ using namespace BF;
 int test_AsymSimpleString() {
 	UNIT_TEST_START;
 	int result = 0;
-	int max = 1;
+	int max = 2 << 8;
 
 	while (!result && max--) {
 		Cipher * c = Cipher::create(kCipherTypeAsymmetric);
@@ -58,13 +58,56 @@ int test_AsymSimpleString() {
 	return result;
 }
 
+int test_AsymLongString() {
+	UNIT_TEST_START;
+	int result = 0;
+	int max = 1;
+
+	while (!result && max--) {
+		Cipher * c = Cipher::create(kCipherTypeAsymmetric);
+		if (c == 0) {
+			result = 1;
+		} else if (c->init()) {
+			result = 2;
+		}
+
+		String str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+		Data plain(str);
+		Data enc;
+		if (!result) {
+			result = c->encrypt(plain, enc);
+		}
+
+		Data dec;
+		if (!result) {
+			result = c->decrypt(enc, dec);
+		}
+
+		if (!result) {
+			const char * res = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+			if (strlen(res) != str.length()) {
+				result = 3;
+			} else if (str.compareString(res)) {
+				printf("%s != %s\n", res, str);
+				result = 2;
+			}
+		}
+
+		c->deinit();
+		BFDelete(c);
+	}
+
+	UNIT_TEST_END(!result, result);
+	return result;
+}
 
 void cipherasymmetric_tests(int * pass, int * fail) {
 	int p = 0, f = 0;
 	
 	INTRO_TEST_FUNCTION;
 
-	LAUNCH_TEST(test_AsymSimpleString, p, f);
+	//LAUNCH_TEST(test_AsymSimpleString, p, f);
+	LAUNCH_TEST(test_AsymLongString, p, f);
 
 	if (pass) *pass += p;
 	if (fail) *fail += f;
