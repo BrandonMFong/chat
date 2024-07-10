901d217979c7798313298ed51f7d0dcd60ef9752
Brando <fong.m.brandon97@gmail.com>
2024-07-10 11:07:24 -0700

    can encrypt/decrypt

--- src/cipherasymmetric.cpp
+++ src/cipherasymmetric.cpp
@@ -33,6 +33,75 @@ CipherType CipherAsymmetric::type() {
 
 /* A property query used for selecting algorithm implementations. */
 static const char *propq = NULL;
+/*
+ * Generates an RSA public-private key pair and returns it.
+ * The number of bits is specified by the bits argument.
+ *
+ * This uses the long way of generating an RSA key.
+ */
+static EVP_PKEY *generate_rsa_key_long(OSSL_LIB_CTX *libctx, unsigned int bits)
+{
+    EVP_PKEY_CTX *genctx = NULL;
+    EVP_PKEY *pkey = NULL;
+    unsigned int primes = 2;
+
+    /* Create context using RSA algorithm. "RSA-PSS" could also be used here. */
+    genctx = EVP_PKEY_CTX_new_from_name(libctx, "RSA", propq);
+    if (genctx == NULL) {
+        fprintf(stderr, "EVP_PKEY_CTX_new_from_name() failed\n");
+        goto cleanup;
+    }
+
+    /* Initialize context for key generation purposes. */
+    if (EVP_PKEY_keygen_init(genctx) <= 0) {
+        fprintf(stderr, "EVP_PKEY_keygen_init() failed\n");
+        goto cleanup;
+    }
+
+    /*
+     * Here we set the number of bits to use in the RSA key.
+     * See comment at top of file for information on appropriate values.
+     */
+    if (EVP_PKEY_CTX_set_rsa_keygen_bits(genctx, bits) <= 0) {
+        fprintf(stderr, "EVP_PKEY_CTX_set_rsa_keygen_bits() failed\n");
+        goto cleanup;
+    }
+
+    /*
+     * It is possible to create an RSA key using more than two primes.
+     * Do not do this unless you know why you need this.
+     * You ordinarily do not need to specify this, as the default is two.
+     *
+     * Both of these parameters can also be set via EVP_PKEY_CTX_set_params, but
+     * these functions provide a more concise way to do so.
+     */
+    if (EVP_PKEY_CTX_set_rsa_keygen_primes(genctx, primes) <= 0) {
+        fprintf(stderr, "EVP_PKEY_CTX_set_rsa_keygen_primes() failed\n");
+        goto cleanup;
+    }
+
+    /*
+     * Generating an RSA key with a number of bits large enough to be secure for
+     * modern applications can take a fairly substantial amount of time (e.g.
+     * one second). If you require fast key generation, consider using an EC key
+     * instead.
+     *
+     * If you require progress information during the key generation process,
+     * you can set a progress callback using EVP_PKEY_set_cb; see the example in
+     * EVP_PKEY_generate(3).
+     */
+    fprintf(stdout, "Generating RSA key, this may take some time...\n");
+    if (EVP_PKEY_generate(genctx, &pkey) <= 0) {
+        fprintf(stderr, "EVP_PKEY_generate() failed\n");
+        goto cleanup;
+    }
+
+    /* pkey is now set to an object representing the generated key pair. */
+
+cleanup:
+    EVP_PKEY_CTX_free(genctx);
+    return pkey;
+}
 
 /*
  * Generates an RSA public-private key pair and returns it.
@@ -176,7 +245,7 @@ cleanup:
 }
 
 int CipherAsymmetric::init() {
-	this->_keys = generate_rsa_key_short(this->_libctx, 4096);
+	this->_keys = generate_rsa_key_long(this->_libctx, 4096);
 	return 0;
 }
 
@@ -274,9 +343,9 @@ static int do_encrypt(OSSL_LIB_CTX *libctx, EVP_PKEY *pub_key,
         fprintf(stderr, "EVP_PKEY_CTX_new_from_pkey() failed.\n");
         goto cleanup;
     }
-    set_optional_params(params, propq);
+    //set_optional_params(params, propq);
     /* If no optional parameters are required then NULL can be passed */
-    if (EVP_PKEY_encrypt_init_ex(ctx, params) <= 0) {
+    if (EVP_PKEY_encrypt_init(ctx) <= 0) {
         fprintf(stderr, "EVP_PKEY_encrypt_init_ex() failed.\n");
         goto cleanup;
     }
@@ -335,7 +404,7 @@ static int do_decrypt(OSSL_LIB_CTX *libctx, EVP_PKEY *priv_key, const unsigned c
     /* The parameters used for encryption must also be used for decryption */
     set_optional_params(params, propq);
     /* If no optional parameters are required then NULL can be passed */
-    if (EVP_PKEY_decrypt_init_ex(ctx, params) <= 0) {
+    if (EVP_PKEY_decrypt_init(ctx) <= 0) {
         fprintf(stderr, "EVP_PKEY_decrypt_init_ex() failed.\n");
         goto cleanup;
     }
